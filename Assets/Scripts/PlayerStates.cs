6d427675e4d3d80f6b040c5c9735cc382af0d2f2
Gonzalo Solís Campos <gsolis95@gmail.com>
2023-03-28 21:00:30 +0200

    Update PlayerStates.cs
    Comentario cambiado

--- Assets/Scripts/PlayerStates.cs
+++ Assets/Scripts/PlayerStates.cs
@@ -1,29 +1,32 @@
-// Voy a hacer una state Machine que controle los ditintos estados del usuario, de esta forma podemos correr codigo que sea unico para cada estado. Esto tiene varias ventajas 
-//y aprovecha principios de la programacion orientada a objetos como la polimorfia. Te dejo un link de referencia por si quieres estudiar mas sobre esto
-//------------> https://gameprogrammingpatterns.com/state.html
+// Voy a hacer una state Machine que controle los ditintos estados del usuario, de esta forma podemos correr codigo que sea unico para cada estado. Esto tiene varias ventajas y aprovecha principios de la programacion orientada a objetos como la polimorfia.
+
+//-------LINK DE CONSULTA-----> https://gameprogrammingpatterns.com/state.html
 //Entre otras cosas nos hace tener un codigo mas limpio y con conocimiento de que se esta ejecutando en cada momento
-
-
 //Si ves algo que no te suena dime
-
 using UnityEngine;
 
-public enum PlayerStatesEnum{PlayerJumping, PlayerGrounded}
-public abstract class PlayerStates
+public enum PlayerStatesEnum
 {
-    public virtual void Tick(){
-
-    }
-    public virtual void OnBegin(){
-
-    }
-    public virtual void OnEnd(){
-
-    }
-
+    PlayerJumping,
+    PlayerGrounded
 }
 
-public  class PlayerJumping : PlayerStates
+public abstract class PlayerStates
+{
+    public virtual void Tick()
+    {
+    }
+
+    public virtual void OnBegin()
+    {
+    }
+
+    public virtual void OnEnd()
+    {
+    }
+}
+
+public class PlayerJumping : PlayerStates
 {
     public override void OnBegin()
     {
@@ -45,7 +48,8 @@ public  class PlayerJumping : PlayerStates
         return base.ToString();
     }
 }
-public  class PlayerGrounded : PlayerStates
+
+public class PlayerGrounded : PlayerStates
 {
     public override void OnBegin()
     {
